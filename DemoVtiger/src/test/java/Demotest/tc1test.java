d127e2b05074b0ee1478fc0e9eae0c747fe7315e
yogeshwar reddy <yogeshwar reddy@LAPTOP-DOM0DU3M>
2023-11-16 10:01:43 +0530

    my first commit

---
+++ DemoVtiger/src/test/java/Demotest/tc1test.java
@@ -0,0 +1,14 @@
+package Demotest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc1test {
+	
+	@Test
+	public void m1()
+	{
+		Reporter.log("haiii bro chill up", true);
+	}
+
+}
