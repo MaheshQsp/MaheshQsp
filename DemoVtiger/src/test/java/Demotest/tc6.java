546b6b9e143725fde177f28f872bcacf46e72729
yogeshwar reddy <yogeshwar reddy@LAPTOP-DOM0DU3M>
2023-11-18 09:42:42 +0530

    eng1 fifth commit

---
+++ DemoVtiger/src/test/java/Demotest/tc6.java
@@ -0,0 +1,14 @@
+package Demotest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class tc6 {
+
+	
+	@Test
+	public void testcase6()
+	{
+		Reporter.log("testcase 6 running",true);
+	}
+}
